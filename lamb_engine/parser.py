1b951813f43689b047418e76318350272baffd1e
Mark <mark@betalupi.com>
2022-11-12 19:14:58 -0800

    Fixed command parser

--- lamb_engine/parser.py
+++ lamb_engine/parser.py
@@ -56,7 +56,7 @@ class LambdaParser:
 			(self.lp + self.pp_history + self.rp)
 		)
 
-		self.pp_command = pp.Suppress(":") + pp.Word(pp.alphas + "_") + pp.Word(pp.alphas + pp.nums + "_.")[0, ...]
+		self.pp_command = pp.Suppress(":") + pp.Word(pp.alphas + "_") + pp.Word(pp.printables)[0, ...]
 
 
 		self.pp_all = (
